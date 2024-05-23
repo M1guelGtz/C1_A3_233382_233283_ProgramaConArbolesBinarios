ba8a767f1b7b5a1a3ce436c7c88879fcaaea4f26
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-05-23 02:29:17 -0600

    [update Add number of products by code]

--- ArbolBinario.java
+++ ArbolBinario.java
@@ -9,20 +9,19 @@ public class ArbolBinario {
 
     public void visualizarMenu() {
         int opc;
-        Scanner scanner = new Scanner(System.in);
         Nodo raiz = null;
         Recorrido r = new Recorrido();
         do {
             System.out.println("\n----------------Menu--------------\n");
-            System.out.println("1. Agregar producto\n2. Actualizar cantidad de producto\n3. Ver lista de Productos\n4. Salir");
+            System.out.println("1. Agregar nuevo producto\n2. Añadir existencia de producto\n3. Ver lista de Productos\n4. Salir");
             System.out.print("eleccion:");
-            opc = decidir();
+            opc = decidirInt();
             switch (opc) {
                 case 1:
                     raiz = agregarProducto(raiz);
                     break;
                 case 2:
-                    //actualizar
+                    actualizarCantidad(raiz);
                     break;
                 case 3:
                     if (raiz == null) {
@@ -40,7 +39,7 @@ public class ArbolBinario {
                     System.out.println("Saliendo...");
                     break;
                 default:
-                    System.out.println("\nSeleccione una opcion con su resectivo indice. \n");
+                    System.out.println("\nSeleccione una opcion con su respectivo indice. \n");
             }
         } while (opc != 4);
     }
@@ -55,31 +54,35 @@ public class ArbolBinario {
             }
             valor++;
             System.out.print("ID del producto:");
-            id = decidir();
+            id = decidirInt();
         } while (id < 100 || id > 999);
         Recorrido r = new Recorrido();
         r.añadirPiezas(raiz, id);
-        
-        System.out.print("Nombre del producto: ");
-        String nombre = scanner.nextLine();
-        System.out.print("Piezas a añadir de" + nombre +  ": ");
-        int cantidad = decidir();
-        System.out.print("Ingrese el precio para " + nombre +  ": ");
-        float precio = scanner.nextFloat();
-        
-        Producto producto = new Producto(id, nombre, cantidad, precio);
-        
-        if (raiz == null) {
-            return new Nodo(producto);
-        } else {
-            insertarNodo(raiz, producto);
+        if (r.evaluar()==0) {
+            
+            System.out.print("Nombre del producto: ");
+            String nombre = scanner.nextLine();
+            System.out.print("Piezas a añadir de " + nombre +  ": ");
+            int cantidad = decidirInt();
+            System.out.print("Ingrese el precio para " + nombre +  ": ");
+            float precio = decidirFloat();
+            
+            Producto producto = new Producto(id, nombre, cantidad, precio);
+            
+            if (raiz == null) {
+                return new Nodo(producto);
+            } else {
+                insertarNodo(raiz, producto);
+            }
         }
         return raiz;
     }
-
     public void insertarNodo(Nodo nodo, Producto producto) {
-        System.out.println("¿Enviar el producto a la izquierda (1) o a la derecha (2) del nodo con ID " + nodo.getDato().getId() + "?");
-        int direccion = decidir();
+        int direccion;
+        do{
+            System.out.println("¿Enviar el producto a la izquierda (1) o a la derecha (2) de " + nodo.getDato().getNombre() + "?");
+            direccion = decidirInt();
+        }while(direccion<1 || direccion > 2);
         
         if (direccion == 1) {
             if (nodo.getIzq() == null) {
@@ -95,7 +98,7 @@ public class ArbolBinario {
             }
         }
     }
-    public int decidir(){
+    public int decidirInt(){
         int indice = 0;
         boolean excepcion = true;
         do{
@@ -113,7 +116,51 @@ public class ArbolBinario {
     } 
     public int validarEleccion() throws InputMismatchException{
         Scanner entrada = new Scanner(System.in);
-        int eleccion = entrada.nextInt();
+        int eleccion, count=0;
+        do{
+            if (count>0) {
+                System.out.println("¡no deben ser numeros negativos!");
+                System.out.print("vuelva a introducir: ");
+            }
+            count++; 
+            eleccion = entrada.nextInt();
+        }while(eleccion<=0);
+        return eleccion;
+    }
+    public void actualizarCantidad(Nodo raiz){
+        Recorrido r = new Recorrido();
+        System.out.println("Ingrese id del Producto;");
+        int id = decidirInt();
+        r.añadirPiezas(raiz, id);
+    }
+    public float decidirFloat(){
+        float indice = 0;
+        boolean excepcion = true;
+        do{
+            try{
+                indice = validarEleccionF();
+                excepcion = false;
+            }catch(InputMismatchException e){
+                System.out.println("\n¡opcion erronea, debe introducir un numero entero!\n");
+                System.out.print("Vuelva a introducir la opcion: ");
+            }
+            
+        }while(excepcion);
+        
+        return indice;
+    } 
+    public float validarEleccionF() throws InputMismatchException{
+        Scanner entrada = new Scanner(System.in);
+        int count=0;
+        Float eleccion;
+        do{
+            if (count>0) {
+                System.out.println("¡no deben ser numeros negativos!");
+                System.out.print("vuelva a introducir: ");
+            }
+            count++; 
+            eleccion = entrada.nextFloat();
+        }while(eleccion<=0);
         return eleccion;
     }
 }
--- Recorrido.java
+++ Recorrido.java
@@ -1,8 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Recorrido {
+    int cantidad = 0;
     public void preorden(Nodo nodo) {
         if (nodo != null) {
-            System.out.println(nodo.getDato().getNombre()+ "\t en existencia: " + nodo.getDato().getCantidad() + " piezas");
+            System.out.println("Id: " + nodo.getDato().getId() + "\t  Nombre: " + nodo.getDato().getNombre()+ "\t en existencia: " + nodo.getDato().getCantidad() + " piezas \t  Precio: $" + nodo.getDato().getPrecio());
             preorden(nodo.getIzq());
             preorden(nodo.getDer());
         }
@@ -20,24 +22,56 @@ public class Recorrido {
         if (nodo != null) {
             postorden(nodo.getIzq());
             postorden(nodo.getDer());
-            System.out.println(nodo.getDato().getNombre() + " (" + nodo.getDato().getCantidad() + ")");
         }
     }
     public void añadirPiezas(Nodo nodo, int id){
-        Scanner scanner = new Scanner(System.in);
+        int opcion;
         if (nodo != null) {
             if (nodo.getDato().getId()==id) {
-                System.out.println("El producto ya existe, ¿desea añadir mas piezas? (1.-SI 2.-NO)");
-                int opcion = scanner.nextInt();
+                System.out.println("El producto existe, ¿desea añadir mas piezas? (1.-SI 2.-NO)");
+                opcion = decidirInt();
                 if (opcion == 1) {
                     System.out.println("¿cuantas piezas desea añadir?");
-                    int cantidad = scanner.nextInt();
+                    cantidad = decidirInt();
                     nodo.getDato().setCantidad( nodo.getDato().getCantidad() + cantidad); 
                 }
+                cantidad = 1;
+            }else{
+                System.out.println("Id unico");
             }
-            preorden(nodo.getIzq());
-            preorden(nodo.getDer());
+            añadirPiezas(nodo.getIzq(), id);
+            añadirPiezas(nodo.getDer(), id);
         }
+    }
+    public int evaluar(){
+        return cantidad;
+    }
+    public int decidirInt(){
+        int indice = 0;
+        boolean excepcion = true;
+        do{
+            try{
+                indice = validarEleccion();
+                excepcion = false;
+            }catch(InputMismatchException e){
+                System.out.println("\n¡opcion erronea, debe introducir un numero entero!\n");
+                System.out.print("Vuelva a introducir la opcion: ");
+            }
+            
+        }while(excepcion);
         
+        return indice;
+    } 
+    public int validarEleccion() throws InputMismatchException{
+        Scanner entrada = new Scanner(System.in);
+        int eleccion, count =0 ;
+        do{
+            if (count>0) {
+                System.out.println("¡no deben ser numeros negativos!");
+            }
+            count++; 
+            eleccion = entrada.nextInt();
+        }while(eleccion<=0);
+        return eleccion;
     }
 }
